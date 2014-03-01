e3c9673141a63edcb1ea47e408fc5a12f1a75cab
Max Keener <maxnkeener@gmail.com>
2014-03-01 11:58:20 -0800

    initial commit

---
+++ src/main/java/com/maxkeener/backathon/Application.java
@@ -0,0 +1,35 @@
+package com.maxkeener.backathon;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: max
+ * Date: 3/1/14
+ * Time: 11:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+}
---
+++ src/main/java/com/maxkeener/backathon/controller/DogeController.java
@@ -0,0 +1,15 @@
+package com.maxkeener.backathon.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: max
+ * Date: 3/1/14
+ * Time: 11:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RestController
+public class DogeController {
+
+
+}
